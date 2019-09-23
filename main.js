6fa688d227df9fce954713d34767dd8dca3a7328
Scott Daniels <sdaniels6363@gmail.com>
2019-09-23 10:53:07 -0400

    API calls for bandsInTown are drafted, still require API key and selection of data.  Function created to handle special characters.

--- main.js
+++ main.js
@@ -10,3 +10,70 @@ var spotifyId = process.env.SPOTIFY_ID;
 var spotifyKey = process.env.SPOTIFY_KEY;
 
 
+function specialCharsBandsInTown(artist) {
+  // checks for special characters in the artist name
+  if (artist.includes("/")) {
+    artist = artist.split("/"); // split on symbol
+    artist = artist.join("%252F"); // rejoin on the required text via API
+  }
+  if (artist.includes("?")) {
+    artist = artist.split("?");// split on symbol
+    artist = artist.join("%253F");// rejoin on the required text via API
+  }
+  if (artist.includes("*")) {
+    artist = artist.split("*");// split on symbol
+    artist = artist.join("%252A");// rejoin on the required text via API
+  }
+  if (artist.includes('"')) {
+    artist = artist.split('"');// split on symbol
+    artist = artist.join("%27C");// rejoin on the required text via API
+  }
+  if (artist.includes(" ")) {
+    artist = artist.split(" ");
+    artist = artist.join("%20");
+  }
+  return artist;
+}
+
+
+function bandsInTownArtist(artist) {
+
+  var artistName = specialCharsBandsInTown(artist);
+
+  axios.get(`https://rest.bandsintown.com/artists/${artistName}`).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  })
+}
+
+function bandsInTownEvents(artist) {
+  var artistName = specialCharsBandsInTown(artist);
+
+  axios.get(`https://rest.bandsintown.com/artists/${artistName}/events`).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  })
+
+
+
+}
