50e4e9f7fd84ed260a117153ca53b195b65e0dc6
Scott Daniels <sdaniels6363@gmail.com>
2019-09-23 10:12:33 -0400

    Added lines to import the API keys from the .env file.

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+// Load Requirements
 var axios = require("axios");
 var moment = require("moment");
+var dotenv = require("dotenv").config();
+
+// Load API Keys
+var omdbAPI = process.env.OMDB_KEY;
+var bandsInTownAPI = process.env.BANDS_KEY;
+var spotifyId = process.env.SPOTIFY_ID;
+var spotifyKey = process.env.SPOTIFY_KEY;
+
+
